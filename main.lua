f6ce7d49fc7c5f85ca06c6eb64b86457da2eda74
EduardaAChagas <adraudech@gmail.com>
2019-09-14 13:20:29 -0300

    first commit

---
+++ main.lua
@@ -0,0 +1,34 @@
+function love.load()
+
+end
+
+
+function love.draw()
+	
+end
+
+
+
+function love.update(dt)
+	
+end
+
+
+
+function love.keypressed()
+	
+end
+
+
+
+
+function love.keyreleased()
+	
+end
+
+
+
+
+function love.event()
+	
+end
